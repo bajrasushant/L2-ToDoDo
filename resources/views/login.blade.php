fde0da144163a8be4d3bff0228c07df351cf2f26
Yakumaa <maharjanshrish8@gmail.com>
2023-09-26 00:24:28 +0545

    implemented login and register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,13 +6,32 @@
 
 <main class="main">
   <div class="container">
+    <div class="message">
+      @if ($errors->any())
+        <div class="error">
+          @foreach ($errors->all() as $error)
+            <div class="alert">{{$error}}</div>
+          @endforeach
+        </div>
+      @endif
+
+      @if (session()->has('error'))
+        <div class="alert">{{session('error')}}</div>
+      @endif
+
+      @if (session()->has('success'))
+        <div class="alert">{{session('success')}}</div>
+      @endif
+    </div>
+    
     <section class="wrapper">
       <div class="heading">
         <h1>Sign In</h1>
         <p>New user? <span><a href="#">Create an account</a></span>
         </p>
       </div>
-      <form action = "#" method= "post" class="form">
+      <form action = "{{ route('login.post') }}" method= "POST" class="form">
+        @csrf
         <div class="input-control">
           <label for="email" class="input-label" hidden>Email Address</label>
           <input type="email" name="email" id="email" class="input-field" placeholder="Email Address">
@@ -23,9 +42,11 @@
         </div>
         <div class="input-control">
           <a href="#">Forgot Password</a>
-          <input type="submit" name="submit" class="input-submit" value="Sign In" disabled>
+          <input type="submit" name="submit" class="input-submit" value="Sign In">
         </div>
       </form>
     </section>
   </div>
 </main>
+
+@endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,16 +6,35 @@
 
 <main class="main">
   <div class="container">
+    <div class="message">
+      @if ($errors->any())
+        <div class="alert error">
+          @foreach ($errors->all() as $error)
+            <div>{{$error}}</div>
+          @endforeach
+        </div>
+      @endif
+
+      @if (session()->has('error'))
+        <div class="alert error">{{session('error')}}</div>
+      @endif
+
+      @if (session()->has('success'))
+        <div class="alert success">{{session('success')}}</div>
+      @endif
+    </div>
+
     <section class="wrapper">
       <div class="heading">
         <h1>Register a new account</h1>
         <p>Already have an account? <span><a href="#">Sign in</a></span>
         </p>
       </div>
-      <form action = "#" method= "post" class="form">
+      <form action = "{{ route('register.post') }}" method= "POST" class="form">
+        @csrf
         <div class="input-control">
           <label for="name" class="input-label" hidden>Name</label>
-          <input type="text" name="username" placeholder="Username" id="usernameField" class="input-field"/>
+          <input type="text" name="name" placeholder="Username" id="usernameField" class="input-field"/>
         </div>
 
         <div class="input-control">
@@ -28,15 +47,17 @@
           <input type="password" name="password" id="passwordField" class="input-field" placeholder="Password">
         </div>
 
-        <div class="input-control">
+        <!-- <div class="input-control">
           <label for="repeatPassword" class="input-label" hidden>Repeat Password</label>
           <input type="password" name="repeatpassword" id="repeatPasswordField" class="input-field" placeholder="Repeat Password">
-        </div>
+        </div> -->
 
         <div class="input-signup">
-          <input type="submit" name="submit" class="input-submit" value="Sign Up" disabled>
+          <input type="submit" name="submit" class="input-submit" value="Sign Up">
         </div>
       </form>
     </section>
   </div>
 </main>
+
+@endsection
